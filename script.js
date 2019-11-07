e74bf3a3c06e00ddcfccc9077538b386bd7d4bec
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2019-11-07 15:56:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<h1>Test</h1>///
+			);
+	}
+}
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
