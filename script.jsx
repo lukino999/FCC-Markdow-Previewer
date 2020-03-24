473770d7bfe49f7d4bd4d57da47b4ba317050da4
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-03-24 12:14:31 +0000

    Passing main tests

--- script.jsx
+++ script.jsx
@@ -15,11 +15,28 @@ class App extends React.Component {
 		});
 	}
 
+	componentDidMount() {
+		console.log("componentDidMount");
+
+		const fillWhenLoad = ["# Header",
+		"## SubHeader",
+		"[Google](https://www.google.com)\n",
+		"Inline `code`",
+		"```", "var s = \"JavaScript syntax highlighting\";\n\nalert(s);" ,"```",
+		"1. List item",
+		"> Blockquotes are very handy in email to emulate reply text.",
+		"\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")",
+		"\nStrong emphasis, aka bold, with **asterisks** or __underscores__."]
+		this.setState({
+			input: fillWhenLoad.join("\n")
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<Header />
-				<Editor handleChange={this.handleChange}/>
+				<Editor handleChange={this.handleChange} input={this.state.input} />
 				<Preview input={this.state.input}/>
 			</div>
 			);
@@ -51,7 +68,7 @@ class Editor extends React.Component {
 
 	render() {
 		return (
-			<textarea id="editor" onChange={this.props.handleChange} />
+			<textarea id="editor" onChange={this.props.handleChange} value={this.props.input}/>
 			);
 	}
 }
@@ -65,6 +82,7 @@ class Preview extends React.Component {
 
 	render() {
 		const rawMarkup = { __html:  marked(this.props.input, {sanitize: true})};
+		// todo: fiddle with options and add syntax higlighting
 
 		return (
 			<div id="preview" dangerouslySetInnerHTML={rawMarkup}/>
